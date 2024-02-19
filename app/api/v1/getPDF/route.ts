032108bc57dd0ea8b98ff651ef9532bf77196e99
mainak <heellomeelo@gmail.com>
2024-02-19 08:21:03 +0530

    brochure img,bold,<br/> not working

--- app/api/v1/getPDF/route.ts
+++ app/api/v1/getPDF/route.ts
@@ -1,22 +1,25 @@
 import { Onedoc } from "@onedoc/client";
 import { compile } from "@onedoc/react-print";
 import { readFileSync, writeFileSync } from "fs";
-import { Payroll } from "@/components/Payroll";
+import { Brochure } from "@/components/Brochure";
 import { join } from "path";
 import { OfferLetter } from "@/components/OfferLetter";
 
 const onedoc = new Onedoc(process.env.ONEDOC_API_KEY as string);
 
-
 export async function GET() {
   const { file, error } = await onedoc.render({
-    html: await compile(Payroll()),
+    html: await compile(Brochure()),
     test: false,
     assets: [
       {
         path: "./util/util.css",
         content: readFileSync(join(process.cwd(), "./util/util.css")).toString(),
       },
+      {
+          path: "image.png",
+          content:readFileSync("image.png"),
+      }
     ],
   });
 
